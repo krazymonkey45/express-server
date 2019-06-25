ef061dfd504cb237cee93d7a63156ca7577e3bd1
Isaiah <isaiah.adams7895@gmail.com>
2019-06-25 14:11:32 +0430

    added html,body,head, title

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 http.createServer(function(request, response){
         response.writeHead(200, {"content-type": "text/html"});
-        response.write("<!DOCTYPE html >")
+        response.write("<!DOCTYPE html >");
         response.write("<html>");
+        response.write("<head>");
+        response.write("<title> first server </title>");
+        response.write("</head>");
+        response.write("<body>");
+        response.write("Hello VERO!!!");
+        response.write("</body>");
+        response.write("</html>");
 });
